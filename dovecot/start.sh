9ee055c4b7b2f09f9f9522e0a46fd3fddbf1c103
Martin Bock <mail@mnbo.de>
2019-06-20 20:03:14 +0200

    Working on dovecot docker image

--- dovecot/start.sh
+++ dovecot/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+SHOULD_EXIT_EARLY=false
+
 if [ ! -d "/srv/config" ]; then
     echo "/srv/config is not mounted"
-    exit 1
+    SHOULD_EXIT_EARLY=true
 fi
 
 function var_not_set {
     echo "Environment variable $1 is not set"
-    exit 1
+    SHOULD_EXIT_EARLY=true
 }
 
 ########
@@ -27,9 +29,9 @@ if [ -z $DB_USER ]; then
     var_not_set "DB_USER"
 fi
 
-if [ -z $DB_PASSWORD ]; then
-    var_not_set "DB_PASSWORD"
-fi
+# if [ -z $DB_PASSWORD ]; then
+#     var_not_set "DB_PASSWORD"
+# fi
 
 if [ -z $DB_NAME ]; then
     var_not_set "DB_NAME"
@@ -37,11 +39,22 @@ fi
 
 ########
 ##
+## Test if we should return early
+##
+
+if [ $SHOULD_EXIT_EARLY = true ]; then
+    exit 1
+fi
+
+########
+##
 ## Preparation
 ##
 
-cp /srv/config/dovecot{,-sql}.conf /etc/dovecot/
-find /srv/sql/ -type f -name 'dovecot-sql.conf' -exec sed -i "s/host=127.0.0.1 dbname=mum_database user=mum_dovecot_user password=mum_dovecot_password/host=${HOSTNAME} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASSWORD}/g" {} \;
+cp /srv/config/dovecot.conf /etc/dovecot/
+find /etc/dovecot/ -type f \
+    -name 'dovecot-sql.conf' \
+    -exec sed -i "s/host=127.0.0.1 dbname=mum_database user=mum_dovecot_user password=mum_dovecot_password/host=${HOSTNAME} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASSWORD}/g" {} \;
 
 ########
 ##
@@ -55,6 +68,6 @@ find /srv/sql/ -type f -name 'dovecot-sql.conf' -exec sed -i "s/host=127.0.0.1 d
 ## Start
 ##
 
-postfix start &
+exec dovecot -F &
 touch /var/log/mail.log
 tail -f /var/log/mail.log
--- postfix/start.sh
+++ postfix/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+SHOULD_EXIT_EARLY=false
+
 if [ ! -d "/srv/config" ]; then
     echo "/srv/config is not mounted"
-    exit 1
+    SHOULD_EXIT_EARLY=true
 fi
 
 function var_not_set {
     echo "Environment variable $1 is not set"
-    exit 1
+    SHOULD_EXIT_EARLY=true
 }
 
 ########
@@ -27,9 +29,9 @@ if [ -z $DB_USER ]; then
     DB_USER="mum_postfix"
 fi
 
-if [ -z $DB_PASSWORD ]; then
-    var_not_set "DB_PASSWORD"
-fi
+# if [ -z $DB_PASSWORD ]; then
+#     var_not_set "DB_PASSWORD"
+# fi
 
 if [ -z $DB_NAME ]; then
     var_not_set "DB_NAME"
@@ -37,10 +39,21 @@ fi
 
 ########
 ##
+## Test if we should return early
+##
+
+if [ $SHOULD_EXIT_EARLY = true ]; then
+    exit 1
+fi
+
+
+########
+##
 ## Preparation
 ##
 
-cp /srv/config/{main,master}.cf /etc/postfix/
+cp /srv/config/main.cf /etc/postfix/
+cp /srv/config/master.cf /etc/postfix/
 find /srv/sql/ -type f -name '*.cf' -exec sed -i "s/mum_postfix_user/${DB_USER}/g" {} \;
 find /srv/sql/ -type f -name '*.cf' -exec sed -i "s/mum_postfix_password/${DB_PASSWORD}/g" {} \;
 find /srv/sql/ -type f -name '*.cf' -exec sed -i "s/mum_postfix_password/${DB_PASSWORD}/g" {} \;
